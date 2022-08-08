7f0439d9cd2b0c062997b865c699a92a72729eea
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-08-08 11:24:39 +0300

    1я здача

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const startBtn = document.querySelector('button[data-start]')
 const stopBtn = document.querySelector('button[data-stop]')
 const  body = document.querySelector('body')
 
-// stopBtn.setAttribute('disabled',true)
+stopBtn.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -12,27 +12,34 @@ function onChangeBody () {
   body.style.backgroundColor = getRandomHexColor ();
 }
 
-
 const colorSwitcher = {
     intervalId: null,
     isActive: false,
    
     start() {
-       if (this.isActive) {
-        startBtn.setAttribute('disabled', true)
-      } 
+      
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+      //  if (this.isActive) {
+      //   startBtn.setAttribute('disabled', true)
+      // } 
       
         // startBtn.setAttribute('disabled', true)
         // stopBtn.removetAttribute('disabled')
-        this.isActive = true;
+      this.isActive = true;
+      
         this.intervalId = setInterval(() => {
           onChangeBody()
         },1000)
     },
 
     stop() {
-        clearInterval(this.intervalId)     
+      clearInterval(this.intervalId)
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
     }
+
 }
 
 startBtn.addEventListener('click', () => {colorSwitcher.start();}) 
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     input: document.querySelector('#datetime-picker'),
@@ -11,61 +11,34 @@ const refs = {
     seconds: document.querySelector('span[data-seconds]'),
 }
 
-
-refs.startBtn.addEventListener('click', () => {
-  const newDate = new Date('Jan 1 2023 00:00:00');
-
-  
-  const timeCount = () => {
-    let now = new Date();
- 
-    let deltaTime  = newDate - now;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = pad(Math.floor(ms / day));
-  const hours = pad(Math.floor((ms % day) / hour));
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-    
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.minutes.textContent = minutes;
-    refs.seconds.textContent = seconds;
-
-  };
-
-  timeCount();
-})
-
-
-
-
-/**
-
-
+refs.startBtn.disabled = true;
 let selectedTime = null;
-//========= Отсчет времени
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = pad(Math.floor(ms / day));
-  const hours = pad(Math.floor((ms % day) / hour));
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-//=== делаем 01 02 ( ане 1,2)
-function pad(value) {
-  return String(value).padStart(2,'0') 
-}
+
+function updateComponentsTimer({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+  }
+
 
 const options = {
   enableTime: true,
@@ -74,8 +47,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
-      window.alert('Please choose a date in the future');
-      selectedDates[0] = new Date();
+      Notify.failure('Please choose a date in the future');
     } else {
       refs.startBtn.disabled = false;
       selectedTime = selectedDates[0];
@@ -83,20 +55,36 @@ const options = {
   },
 };
 
-const timer = {
-    start() {
-        const startTime = Date.now();
-        setInterval(() => {
-            const currentTime = Date.now();
 
-        },1000)
+const timer = {
+
+    timerID: null,
+    isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
     }
+
+    this.isActive = true;
+    this.timerID = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = selectedTime - currentTime;
+      const componentsTimer = convertMs(deltaTime);
+     updateComponentsTimer(componentsTimer);
+      if (deltaTime <= 0) {
+        this.stop();
+      }
+    }, 1000);
+  },
+
+
+  stop() {
+    clearInterval(this.timerID);
+  }
 }
 
-timer.start()
+
 
 flatpickr(refs.input, options);
-
-
-
-*/
+refs.startBtn.addEventListener('click', () => timer.start());
